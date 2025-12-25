29ed9318ab98c4d3cf095a1d028fed487f0c063f
Annyfee <2287551746@qq.com>
2025-12-25 19:29:18 +0800

    chore: minor adjustments

--- m10_mcp_basics/stdio_server.py
+++ m10_mcp_basics/stdio_server.py
@@ -12,7 +12,7 @@ async def get_weather(city:str):
     如果是此时此刻的天气请求，调用此工具。
     """
     # 模拟真实的网络请求(你可以换成自己的天气API)
-    # 在MCP中，工具函数可以是async的，FastMCP会自动处理
+    # 在MCP中，工具函数可以是async的，FastMCP会自动处理。
     return f"{city}的天气是:晴，气温25℃，风力3级"
 
 
